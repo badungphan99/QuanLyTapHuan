481a1e7bd57c120e568bedf5f2171c35711f9807
= <=>
2020-11-07 10:26:42 +0700

    "view teacher of course"

--- app/app/Course.php
+++ app/app/Course.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     protected $table = 'course';
+    protected $fillable = [
+        'id',
+        'name',
+        'date',
+        'content',
+        'program',
+        'location',
+        'num_student',
+        'status'
+    ];
     public $timestamps = false;
+    public function teachers()
+    {
+        return $this->hasMany('App\Teacher');
+    }
 }
