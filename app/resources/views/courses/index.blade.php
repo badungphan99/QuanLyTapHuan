efa88b3c954db2e7695cc35462dedfd7b3b6691b
Nhi <quynhnhi0124@gmail.com>
2020-12-06 03:17:33 +0700

    sua gd

--- app/resources/views/courses/index.blade.php
+++ app/resources/views/courses/index.blade.php
@@ -1,32 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Courses Manager</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <!-- Styles -->
-        <style>
-
-        </style>
-    </head>
-    <body>
+@extends('layouts.app')
+<title>Chương trình @yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/program-index.css') }}">
+@section('content')
         <div class="container">
-            <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4">
-                    <h1>Các khóa tập huấn</h1>
-                </div>
-                <div class="col-sm-4"></div>
-            </div>
+            <div class="head-content">
+                <h1 class="display-4">Các khóa tập huấn</h1><br>
+                <a href="/course/create" class="btn btn-lg btn-outline-dark pull-right" id="create_course"><span><i class="fa fa-plus" aria-hidden="true"></i></span> &nbsp Thêm mới</a><br><br>
 
+            </div>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
             <div class="table-responsive">
                 <table class="table table-hover ">
                     <thead>
@@ -53,13 +39,20 @@
                                 <td><label for="">{{ $course->name }}</label></td>
                                 <td><p class="font-weight-light">{{ $course->num_student }}</p></td>
                                 <td><p class="font-weight-light">{{ $course->location }}</p></td>
-                                <td><p class="font-weight-light">{{ $course->status }}</p></td>
                                 <td>
-                                    <a href="/course/edit/{{ $course->id }}" class="btn btn-outline-warning">Chỉnh sửa</a>
-                                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">
+                                    @if ($course->status == 1)
+                                        <p class="font-weight-dark">Active</p>
+                                    @else
+                                        <p class="font-weight-light">Inactive</p>
+                                    @endif
+                                </td>
+                                <td>
+                                    <a href="/course/edit/{{ $course->id }}" class="btn btn-lg btn-outline-dark"><span><i class="fa fa-pencil-square-o" aria-hidden="true"></i></span>&nbsp Chỉnh sửa</a>
+                                    <button type="button" class="btn btn-lg btn-outline-dark" data-toggle="modal" data-target="#exampleModal">
                                        Chi tiết
                                     </button>
-                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+                                    <div class="modal fade" id="exam{{$course->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                         <div class="modal-dialog" role="document">
                                             <div class="modal-content">
                                             <div class="modal-header">
@@ -76,6 +69,8 @@
                                                 <label for=""><b>Ngày khai giảng: </b></label>
                                                 <p class="font-weight-light">{{ $course->date }}</p>
                                                 <label for=""><a href="/course/teacher/{{ $course->id }}">Giảng viên phụ trách</a></label>
+                                                <br>
+                                                <label for=""><a href="/course/student/{{ $course->id }}">Sinh viên đã đăng ký</a></label>
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
@@ -89,7 +84,5 @@
                     </tbody>
                 </table>
             </div>
-            <a href="/course/create" class="btn btn-primary" id="create_course">Thêm mới</a>
         </div>
-    </body>
-</html>
+@endsection
