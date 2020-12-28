d444b686d9f6cbfc174b6357a3d91123f634d15d
dungpb <badungphan99@gmail.com>
2020-12-28 16:45:55 +0700

    update web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\UpdatePasswordController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\Users\UserController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,58 +29,55 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/list-all-user', 'UserController@listAllUser');
+Route::get('/list-all-user', [UserController::class, 'listAllUser']);
+Route::get('/user/{username}', [UserController::class, 'getUserByUserName']);
+Route::get('/disable-user/{username}', [UserController::class, 'disableUser']);
+Route::get('/enable-user/{username}', [UserController::class, 'enableUser']);
+Route::put('/create-new_account/{$data}', [UserController::class, 'creatNewAccount']);
+Route::get('/set-role', [UserController::class, 'setRoleUser']);
+Route::get('/set-info', [UserController::class, 'setInfoUser']);
 
-Route::get('/user/{username}', 'UserController@getUserByUserName');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/disable-user/{username}', 'UserController@disableUser');
+Route::get('/password/reset', [ForgotPasswordController::class, 'showRequestForm'])->name('password.request');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
+Route::get('/password/update', [UpdatePasswordController::class, 'showRequestForm'])->name('password.update.request');
+Route::post('/password/update', [UpdatePasswordController::class, 'updatePassword'])->name('password.update');
 
-Route::get('/enable-user/{username}', 'UserController@enableUser');
-
-Route::put('/create-new_account/{$data}', 'UserController@creatNewAccount');
-
-Route::get('/set-role', 'UserController@setRoleUser');
-
-Route::get('/set-info', 'UserController@setInfoUser');
-Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
-
-Route::get('/password/reset', 'Auth\ForgotPasswordController@showRequestForm')->name('password.request');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.reset');
-Route::get('/password/update', 'Auth\UpdatePasswordController@showRequestForm')->name('password.update.request');
-Route::post('/password/update', 'Auth\UpdatePasswordController@updatePassword')->name('password.update');
-
-Route::get('/superAdmin', 'SuperAdminController@index')->name('superAdmin');
-Route::get('/detail_user', 'SuperAdminController@detail_infor_user')->name('detail_user');
-Route::post('/superAdmin/update_info_user', 'SuperAdminController@update_info_user')->name('update_info_user');
-Route::post('/superAdmin/create-new-account', 'SuperAdminController@create_new_account')->name('create_new_account');
+Route::get('/superAdmin', [SuperAdminController::class, 'index'])->name('superAdmin');
+Route::get('/detail_user', [SuperAdminController::class, 'detail_infor_user'])->name('detail_user');
+Route::post('/superAdmin/update_info_user', [SuperAdminController::class, 'update_info_user'])->name('update_info_user');
+Route::post('/superAdmin/create-new-account', [SuperAdminController::class, 'create_new_account'])->name('create_new_account');
 
 
-Route::get('/user/show', 'Users\UserController@show')->middleware('auth')->name('user.showinfo');
-Route::post('user/update','Users\UserController@update')->middleware('auth')->name('user.updateInfo');
+Route::get('/user/show', [UserController::class, 'show'])->middleware('auth')->name('user.showinfo');
+Route::post('user/update',[UserController::class, 'update'])->middleware('auth')->name('user.updateInfo');
 
-Route::get('/course', 'CourseController@index')->name('index_course');
-Route::get('/course/create', 'CourseController@create')->name('create_course_form');
-Route::post('/course/store', 'CourseController@store')->name('store_course');
-Route::get('/course/edit/{id}', 'CourseController@show')->name('show_course');
-Route::post('/course/edit/{id}', 'CourseController@update')->name('edit_course');
+Route::get('/course', [CourseController::class, 'index'])->name('index_course');
+Route::get('/course/create', [CourseController::class, 'create'])->name('create_course_form');
+Route::post('/course/store', [CourseController::class, 'store'])->name('store_course');
+Route::get('/course/edit/{id}', [CourseController::class, 'show'])->name('show_course');
+Route::post('/course/edit/{id}', [CourseController::class, 'update'])->name('edit_course');
 
-Route::get('/course/teacher/{id}', 'CourseController@view_teacher')->name('view_teacher');
-Route::post('/teacher/store/{id}', 'CourseController@store_teacher')->name('store_teacher');
-Route::get('/teacher/delete/{id}', 'CourseController@delete_teacher')->name('delete_teacher');
+Route::get('/course/teacher/{id}', [CourseController::class, 'view_teacher'])->name('view_teacher');
+Route::post('/teacher/store/{id}', [CourseController::class, 'store_teacher'])->name('store_teacher');
+Route::get('/teacher/delete/{id}', [CourseController::class, 'delete_teacher'])->name('delete_teacher');
 
-Route::get('/course/student/{id}', 'CourseController@view_student')->name('view_student');
-Route::post('/student/store/{id}', 'CourseController@store_student')->name('store_student');
-Route::get('/student/delete/{id}', 'CourseController@delete_student')->name('delete_student');
+Route::get('/course/student/{id}', [CourseController::class, 'view_student'])->name('view_student');
+Route::post('/student/store/{id}', [CourseController::class, 'store_student'])->name('store_student');
+Route::get('/student/delete/{id}', [CourseController::class, 'delete_student'])->name('delete_student');
 
-Route::get('/program', 'ProgramController@index')->name('index_program');
-Route::get('/program/create', 'ProgramController@create')->name('create_program_form');
-Route::post('/program/store', 'ProgramController@store')->name('store_program');
-Route::get('/program/edit/{id}', 'ProgramController@show')->name('show_program');
-Route::post('/program/edit/{id}', 'ProgramController@update')->name('edit_program');
-Route::get('/program/courses/{id}', 'ProgramController@view_course')->name('view_course');
-Route::get('/', 'WelcomeController@index')->name('welcome');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home/register/{id}', 'HomeController@register_course')->name('register_course');
+Route::get('/program', [ProgramController::class, 'index'])->name('index_program');
+Route::get('/program/create', [ProgramController::class, 'create'])->name('create_program_form');
+Route::post('/program/store', [ProgramController::class, 'store'])->name('store_program');
+Route::get('/program/edit/{id}', [ProgramController::class, 'show'])->name('show_program');
+Route::post('/program/edit/{id}', [ProgramController::class, 'update'])->name('edit_program');
+Route::get('/program/courses/{id}', [ProgramController::class, 'view_course'])->name('view_course');
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home/register/{id}', [HomeController::class, 'register_course'])->name('register_course');
+
+
 // Route::get('/course', 'SuperAdminController@detail_infor_user')->name('index_course');
 
 // Route::get('/course/edit/{id}', 'CourseController@show')->name('show_course');
