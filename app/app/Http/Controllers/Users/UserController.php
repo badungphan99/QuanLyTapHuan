d2accd2e67eabaf5c14d0e4d66b7c378254492cd
dungpb <badungphan99@gmail.com>
2021-01-06 21:31:05 +0700

    update view info

--- app/app/Http/Controllers/Users/UserController.php
+++ app/app/Http/Controllers/Users/UserController.php
@@ -14,28 +14,42 @@ class UserController
         if(Auth::check()){
             $id = Auth::id();
             $user = User::find($id);
-            return die($user->other_email);
         }else{
             return view('login');
         }
     }
 
+    public function showDetailForm(){
+        if (Auth::check()) {
+            return view('auth.detail_user')->with('user', json_decode(Auth::user(), true));
+        }else{
+            return redirect("/home");
+        }
+    }
+
     public function update(Request $request){
-        $this->validator($request->all())->validate();
         $id = Auth::id();
-        $user = User::find($id);
+        $user = User::where('id',$id);
         $user->update([
             'fullname' => $request['fullname'],
             'unit' => $request['unit'],
             'other_email' => $request['other_email'],
             'phone_number' => $request['phone_number'],
             'other_phone_number' => $request['other_phone_number'],
-            'postition' => $request['postition'],
+            'position' => $request['position'],
             'academic_rank' => $request['academic_rank'],
             'academic_degree' => $request['academic_degree'],
+            'note' => $request['note'],
         ]);
+        return redirect('/detail_user');
     }
 
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
     protected function validator(array $data){
         return Validator::make($data,[
             'fullname' => ['required', 'string', 'max:255'],
